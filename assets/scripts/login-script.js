ea024931efedd21a031019acd67865513c8d7611
ArdaFauzan <107672285+ArdaFauzan@users.noreply.github.com>
2023-08-23 23:24:54 +0700

    Mengganti komentar dengan JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,42 +1,62 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
+/**
+ * @module Login-Script
+/**
+ * Membuat variabel loginFormElement untuk tampilan form
+ * @constant {HTMLElement} 
+*/
+const loginFormElement = document.querySelector('#loginForm');
 
 
 /**
- * Membuat variabel untuk setiap elemen view
+ * Membuat variabel inputEmailElement untuk tampilan input email
+ * @constant {HTMLElement}
  */
-const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+ * Membuat variabel inputPasswordElement untuk tampilan input password
+ * @constant {HTMLElement}
+ */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+
 /**
- * Membuat variabel untuk menyimpan informasi email dan password
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara
+ * @constant {string}
  */
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
+
+
 
 /**
  * Menambahkan aksi klik pada button
  */
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
 
     /**
-     * Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan
+     * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan
+     * @constant {string}
      */
     const email = inputEmailElement.value;
+
+
+    /**
+     * Membuat password untuk menyimpan nilai password yang didapatkan saat button ditekan
+     * @constant {string}
+     */
     const password = inputPasswordElement.value;
   
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
-
+  
     /**
      * Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan
      */
